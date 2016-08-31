69ddb198cd1f800b2548427dc57cec920cacfc49
Christian Schnelting <c.schnelting@evosec.de>
2016-08-31 15:49:03 +0200

    merge if-statements

--- src/de/evosec/fotilo/MainActivity.java
+++ src/de/evosec/fotilo/MainActivity.java
@@ -30,10 +30,8 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			if (checkAndRequestPermissions()) {
-				startCameraFragment();
-			}
+		if (savedInstanceState == null && checkAndRequestPermissions()) {
+			startCameraFragment();
 		}
 	}
 
