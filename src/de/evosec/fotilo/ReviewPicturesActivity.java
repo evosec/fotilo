ae4397bf16d99adc06f92ca94bba4c1b6dcfad77
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-09-20 13:35:08 +0200

    Extend Activity

--- src/de/evosec/fotilo/ReviewPicturesActivity.java
+++ src/de/evosec/fotilo/ReviewPicturesActivity.java
@@ -9,13 +9,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 
-public class ReviewPicturesActivity extends AppCompatActivity
+public class ReviewPicturesActivity extends Activity
         implements View.OnClickListener {
 
 	private static final Logger LOG =
