95be16808e1c9620e464c72f45b8d6a5572c3fe0
Kay-Uwe Janssen <k.janssen@herbrand.de>
2018-01-18 14:55:41 +0100

    remove unnecessary override

--- fotilo-app/src/main/java/de/evosec/fotilo/MainActivity.java
+++ fotilo-app/src/main/java/de/evosec/fotilo/MainActivity.java
@@ -1,15 +1,7 @@
 package de.evosec.fotilo;
 
-import android.os.Bundle;
-
 import de.evosec.fotilo.library.FotiloActivity;
 
 public class MainActivity extends FotiloActivity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-	}
 
 }
